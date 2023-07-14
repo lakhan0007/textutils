b64210b915bd9d22fd6a7b8458bbc5e9c0cc9dc5
lakhan singh rathore <107997584+lakhan0007@users.noreply.github.com>
2023-07-15 03:14:29 +0530

    darkmode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import "./App.css";
 // import About from "./components/About";
 import Form from "./components/Form";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const [mode, setMode] = useState('dark');
+
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042734';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-      <Navbar title="TEXTUTILS" about="ABOUTUS"></Navbar>
+      <Navbar title="TEXTUTILS" about="ABOUTUS" mode={mode} toggleMode={toggleMode}></Navbar>
       <div className="container my-3" >
-        <Form heading="Enter the text to analyze below" />
+        <Form heading="Enter the text to analyze below" mode={mode}/>
         {/* <About About="About Us"></About> */}
       </div>
     </>
--- src/components/Form.js
+++ src/components/Form.js
@@ -23,6 +23,10 @@ export default function Form(props) {
       text.select();
       navigator.clipboard.writeText(text.value);
 }
+   const handelExtraSpaceRemove = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "))
+}
 
     const handelOnChange = (e) => {
         setText(e.target.value)
@@ -32,20 +36,21 @@ export default function Form(props) {
 
   return (
     <>
-      <div className="container">
+      <div className="container" style={{color: props.mode==='dark' ? 'white':'#042734'}}>
         <h2>{props.heading}</h2>
-        <textarea className="form-control my-3" id="myBox" onChange={handelOnChange} rows="8" value={text}></textarea>
+        <textarea className="form-control my-3" id="myBox" onChange={handelOnChange} style={{backgroundColor: props.mode==='dark' ? 'gray':'white', color: props.mode==='dark' ? 'white':'#042734'}} rows="8" value={text}></textarea>
         <button className="btn btn-primary mx-2" onClick={handelUpClick}> Convart to Uppercase</button>
         <button className="btn btn-secondary mx-2" onClick={handelLcClick} > Convart to Lowercase</button> 
         <button className="btn btn-danger mx-2" onClick={handelClearClick} > Clear text</button> 
         <button className="btn btn-success mx-2" onClick={handelCopy} > Copy text</button> 
+        <button className="btn btn-dark mx-2" onClick={handelExtraSpaceRemove} >Space Remove</button> 
       </div>
-      <div className="container my-4">
+      <div className="container my-4" style={{color: props.mode==='dark' ? 'white':'#042734'}}>
         <h1>Your text summary</h1>
         <p>{text.split(" ").length} words and {text.length} characters</p>
         <p>{0.008 * text.length} Minutes read</p>
         <h3>Preview</h3>
-        <p>{text}</p>
+        <p>{text.length>0?text:'Enter something in the textbox above the preview it here'}</p>
       </div>
     </>
   );
